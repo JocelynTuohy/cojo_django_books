7671798e0f53309d9dafc2c2efe19c5d91ae55ed
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-20 02:26:21 -0700

    completed books assignment (added 5 records, added new table field, and then re-migrated)

--- apps/books/models.py
+++ apps/books/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.CharField(max_length=8)
+    category = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    in_print = models.BooleanField(default=True)
